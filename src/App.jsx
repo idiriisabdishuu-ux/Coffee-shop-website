b583ff046767d1baa1dac7be6ab24d2280d8b363
Idiriis Abdishuu <email_idiriisabdishuu@gmail.com>
2026-09-07 20:56:22 +0300

    Update coffee shop website

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App(){
 
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />}/>
       <Route path="/" element={<Home />}/>
       <Route path="/cart" element={<Cart />}/>
       <Route path="/menu" element={<Menu />} /> 
